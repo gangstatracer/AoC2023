79d043d9797ea05857db25c310f6cd43322183c4
iperevoschikov <iperevoschikov@skbkontur.ru>
2023-12-13 19:47:28 +0500

    Day 13 part 2

--- AoC2023/Day13/Day13.cs
+++ AoC2023/Day13/Day13.cs
@@ -27,23 +27,9 @@ public class Day13
 
         patterns.Add(currentPattern);
 
-        patterns.Select(Score).Sum().Should().Be(expected);
+        patterns.Select(p => Score(p, FindSymmetry)).Sum().Should().Be(expected);
     }
 
-    private static int Score(List<string> pattern)
-    {
-        var vertical = FindSymmetry(pattern);
-        if (vertical >= 0)
-            return vertical;
-        var horizontal = FindSymmetry(Rotate(pattern).ToList());
-        return 100 * horizontal;
-    }
-
-    private static IEnumerable<string> Rotate(IReadOnlyList<string> pattern)
-    {
-        for (var i = 0; i < pattern[0].Length; i++)
-            yield return new string(pattern.Select(l => l[i]).ToArray());
-    }
 
     private static int FindSymmetry(IReadOnlyList<string> pattern)
     {
@@ -69,5 +55,90 @@ public class Day13
         }
 
         return -1;
+    }
+
+    [TestCase("Day13/example.txt", 400)]
+    [TestCase("Day13/input.txt", 36735)]
+    public void Part2(string path, int expected)
+    {
+        var input = File.ReadLines(path).ToArray();
+        var patterns = new List<List<string>>();
+        var currentPattern = new List<string>();
+        var i = 0;
+        do
+        {
+            if (string.IsNullOrEmpty(input[i]))
+            {
+                patterns.Add(currentPattern);
+                currentPattern = new List<string>();
+            }
+            else
+            {
+                currentPattern.Add(input[i]);
+            }
+
+            i++;
+        } while (i < input.Length);
+
+        patterns.Add(currentPattern);
+
+        patterns.Select(p => Score(p, FindSymmetryWithError)).Sum().Should().Be(expected);
+    }
+
+    private static int FindSymmetryWithError(IReadOnlyList<string> pattern)
+    {
+        var width = pattern[0].Length;
+        var expectedErrorCount = 1;
+        for (var i = 1; i < width; i++)
+        {
+            var palindrome = true;
+            var errorCount = 0;
+            for (var j = 0; j < i; j++)
+            {
+                var left = j;
+                var right = i + i - j - 1;
+                var symmetric =
+                    right >= width
+                    || pattern.All(l =>
+                    {
+                        if (l[left] == l[right])
+                            return true;
+                        if (errorCount == 0)
+                        {
+                            errorCount++;
+                            return true;
+                        }
+
+                        return false;
+                    });
+
+                if (!symmetric)
+                {
+                    palindrome = false;
+                    break;
+                }
+            }
+
+            if (palindrome && errorCount == 1)
+                return i;
+        }
+
+        return -1;
+    }
+
+    private static int Score(List<string> pattern, Func<List<string>, int> func)
+    {
+        var vertical = func(pattern);
+        if (vertical >= 0)
+            return vertical;
+        var horizontal = func(Rotate(pattern).ToList());
+        return 100 * horizontal;
+    }
+
+
+    private static IEnumerable<string> Rotate(IReadOnlyList<string> pattern)
+    {
+        for (var i = 0; i < pattern[0].Length; i++)
+            yield return new string(pattern.Select(l => l[i]).ToArray());
     }
 }
