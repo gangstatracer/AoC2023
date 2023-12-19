25b7e54a4b85ac662ff77359df645531bbe3da37
iperevoschikov <iperevoschikov@skbkontur.ru>
2023-12-19 13:58:39 +0500

    not working Day18 and Day19 complete

---
+++ AoC2023/Day18/Day18.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+
+namespace AoC2023.Day18;
+
+public class Day18
+{
+    [TestCase("Day18/example.txt", 62)]
+    [TestCase("Day18/input.txt", 8116)]
+    public void Part1(string path, int expected)
+    {
+        var input = File
+            .ReadLines(path)
+            .Select(l =>
+            {
+                var parts = l.Split(" ");
+                return (parts[0][0] switch
+                    {
+                        'R' => (0, 1),
+                        'U' => (-1, 0),
+                        'L' => (0, -1),
+                        'D' => (1, 0),
+                    },
+                    int.Parse(parts[1]),
+                    parts[2].Substring(2, 6));
+            })
+            .ToArray();
+
+        var x = 0;
+        var y = 0;
+        var map = new Dictionary<(int, int), string?> { [(0, 0)] = "000000" };
+        foreach (var (direction, length, color) in input)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                x += direction.Item1;
+                y += direction.Item2;
+                map[(x, y)] = color;
+            }
+        }
+
+        var loopCount = map.Keys.Count;
+
+        var maxX = map.Keys.Select(k => k.Item1).Max();
+        var minX = map.Keys.Select(k => k.Item1).Min();
+        var maxY = map.Keys.Select(k => k.Item2).Max();
+        var minY = map.Keys.Select(k => k.Item2).Min();
+
+        BFS(
+            minX, maxX,
+            minY, maxY,
+            map,
+            minX + 2,
+            minY + 153,
+            "000000");
+
+        var innerCount = 0;
+        var builder = new StringBuilder();
+        for (var i = minX; i <= maxX; i++)
+        {
+            for (var j = minY; j <= maxY; j++)
+            {
+                if (!map.TryGetValue((i, j), out var color))
+                {
+                    innerCount++;
+                    builder.Append('O');
+                }
+                else
+                {
+                    builder.Append(color == "000000" ? '.' : '#');
+                }
+            }
+
+            builder.Append(Environment.NewLine);
+        }
+
+        File.WriteAllText("Day18/output.txt", builder.ToString());
+
+        (loopCount + innerCount)
+            .Should()
+            .Be(expected);
+    }
+
+    private static void BFS(
+        int minX,
+        int maxX,
+        int minY,
+        int maxY,
+        IDictionary<(int, int), string?> map,
+        int startX,
+        int startY,
+        string color)
+    {
+        var queue = new Queue<(int, int)>();
+        queue.Enqueue((startX, startY));
+        while (queue.Count > 0)
+        {
+            var (x, y) = queue.Dequeue();
+            map[(x, y)] = color;
+            for (var i = -1; i < 2; i++)
+            for (var j = -1; j < 2; j++)
+                if (x + i >= minX - 1
+                    && x + i <= maxX + 1
+                    && y + j >= minY - 1
+                    && y + j <= maxY + 1
+                    && !map.ContainsKey((x + i, y + j)))
+                    queue.Enqueue((x + i, y + j));
+        }
+    }
+}
---
+++ AoC2023/Day19/Day19.cs
@@ -0,0 +1,203 @@
+﻿namespace AoC2023.Day19;
+
+public class Day19
+{
+    [TestCase("Day19/example.txt", 19114)]
+    [TestCase("Day19/input.txt", 432434)]
+    public void Part1(string path, int expected)
+    {
+        var input = File.ReadLines(path).ToArray();
+        var workflows = new Dictionary<string, List<Instruction>>();
+        var i = 0;
+        while (!string.IsNullOrEmpty(input[i]))
+        {
+            var (id, rest) = input[i].Halve("{");
+
+            var instructions = rest[..^1]
+                .Split(',')
+                .ToArray();
+            workflows[id] = instructions
+                .Take(instructions.Length - 1)
+                .Select(s =>
+                {
+                    var (condition, workflow) = s.Halve(":");
+                    Func<Gear, int> propertyGetter = condition[0] switch
+                    {
+                        'x' => g => g.X,
+                        'm' => g => g.M,
+                        'a' => g => g.A,
+                        's' => g => g.S,
+                    };
+                    Func<int, int, bool> comparision = condition[1] switch
+                    {
+                        '<' => (a, b) => a < b,
+                        '>' => (a, b) => a > b,
+                    };
+                    var number = int.Parse(condition[2..]);
+                    return new Instruction(
+                        g => comparision(
+                            propertyGetter(g),
+                            number),
+                        workflow);
+                })
+                .Append(new Instruction(_ => true, instructions[^1]))
+                .ToList();
+            i++;
+        }
+
+        i++;
+        var gears = input[i..]
+            .Select(l =>
+            {
+                var parts = l[1..^1]
+                    .Split(',')
+                    .Select(p => p
+                        .Halve("=", null, int.Parse)
+                        .Item2)
+                    .ToArray();
+                return new Gear(X: parts[0], M: parts[1], A: parts[2], S: parts[3]);
+            })
+            .ToArray();
+
+        var sum = 0;
+        foreach (var gear in gears)
+        {
+            var workflowId = "in";
+            var instructionIndex = 0;
+            do
+            {
+                var instruction = workflows[workflowId][instructionIndex];
+                if (instruction.Predicate(gear))
+                {
+                    workflowId = instruction.Result;
+                    instructionIndex = 0;
+                }
+                else
+                {
+                    instructionIndex++;
+                }
+            } while (workflowId != "R" && workflowId != "A");
+
+            if (workflowId == "A")
+            {
+                var (x, m, a, s) = gear;
+                sum += x + m + a + s;
+            }
+        }
+
+        sum.Should().Be(expected);
+    }
+
+    private record Instruction(Func<Gear, bool> Predicate, string Result);
+
+    private record Gear(int X, int M, int A, int S);
+
+    [TestCase("Day19/example.txt", 167409079868000Lu)]
+    [TestCase("Day19/input.txt", 432434Lu)]
+    public void Part2(string path, ulong expected)
+    {
+        var input = File.ReadLines(path).ToArray();
+        var workflows = new Dictionary<string, List<(string, string)>>();
+        var i = 0;
+        while (!string.IsNullOrEmpty(input[i]))
+        {
+            var (id, rest) = input[i].Halve("{");
+
+            var instructions = rest[..^1]
+                .Split(',')
+                .ToArray();
+            workflows[id] = instructions
+                .Take(instructions.Length - 1)
+                .Select(s =>
+                {
+                    var (condition, workflow) = s.Halve(":");
+
+                    return (condition, workflow);
+                })
+                .Append(("", instructions[^1]))
+                .ToList();
+            i++;
+        }
+
+        var paths = new List<(string, List<string>)> { ("in", new List<string>()) };
+        while (paths.Any(t => t.Item1 != "A"))
+        {
+            var nextPaths = paths.Where(t => t.Item1 == "A").ToList();
+            foreach (var p in paths.Where(t => t.Item1 != "A"))
+            {
+                var previousNegated = new List<string>();
+                foreach (var instruction in workflows[p.Item1])
+                {
+                    var conditions = new List<string>(
+                        p
+                            .Item2
+                            .Concat(previousNegated)
+                            .Append(instruction.Item1));
+                    nextPaths.Add((instruction.Item2, conditions));
+                    previousNegated.Add(Negate(instruction.Item1));
+                }
+            }
+
+            paths = nextPaths
+                .Where(t => t.Item1 != "R")
+                .ToList();
+        }
+
+        var combinations = 0LU;
+        foreach (var p in paths)
+        {
+            var limits = new Dictionary<char, (int Min, int Max)>
+            {
+                ['x'] = (0, 4001),
+                ['m'] = (0, 4001),
+                ['a'] = (0, 4001),
+                ['s'] = (0, 4001),
+            };
+            var conditions = p
+                .Item2
+                .Where(c => !string.IsNullOrEmpty(c))
+                .ToList();
+            foreach (var c in conditions)
+            {
+                var (min, max) = limits[c[0]];
+                var number = int.Parse(c[2..]);
+                switch (c[1])
+                {
+                    case '<':
+                        max = number < max ? number : max;
+                        break;
+                    case '>':
+                        min = number > min ? number : min;
+                        break;
+                }
+
+                limits[c[0]] = (min, max);
+            }
+
+            combinations += limits.Aggregate(
+                1Lu,
+                (c, t) =>
+                {
+                    var range = t.Value.Max - t.Value.Min - 1;
+                    if (range < 0)
+                        return 0;
+                    return c * Convert.ToUInt64(range);
+                });
+        }
+
+        combinations.Should().Be(expected);
+    }
+
+    private static string Negate(string condition)
+    {
+        if (string.IsNullOrEmpty(condition))
+            return condition;
+
+        var number = int.Parse(condition[2..]);
+        return condition[1] switch
+        {
+            '<' => "" + condition[0] + '>' + (number - 1),
+            '>' => "" + condition[0] + '<' + (number + 1),
+        };
+    }
+}
